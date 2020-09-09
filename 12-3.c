6978a6b28058cc37fff6b4b66734b897f220ed08
Shinji-Sugasawa <68045030+Shinji-Sugasawa@users.noreply.github.com>
2020-09-09 12:36:55 +0900

    Create 12-3.c

---
+++ 12-3.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int check (int r1, int r2, int r3, int r4)
+{
+	int judge=0; //default:false
+	 if (r1==2 && r2==4 && r3==1 && r4==3)
+		 judge=1; //true
+		 
+		 return judge;
+}
+void draw (int r1, int r2,int r3,int r4)
+{
+	printf (" - Q - -\n");
+	printf (" - - - Q\n");
+	printf (" Q - - -\n");
+	printf (" - - Q -\n");
+}
+int main (void)
+{
+	int i, j, k, m;
+	for(i=1;i<=4; i++)
+	{
+		for(j=1;j<=4;j++)
+		{
+			for(k=1;k<=4;k++)
+			{
+				for(m=1;m<=4;m++)
+				{
+					if (check(i,j,k,m)!=0)
+					{
+						draw(i,j,k,m);
+					}
+				}
+			}
+		}
+	}
+	return 0;
+}
