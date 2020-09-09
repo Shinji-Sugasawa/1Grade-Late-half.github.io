b54825c5c48f07e64369e89508125088b5e8bb61
Shinji-Sugasawa <68045030+Shinji-Sugasawa@users.noreply.github.com>
2020-09-09 12:36:05 +0900

    Create practice11.c

---
+++ practice11.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main (void)
+{
+	int i, me, d1=0,d2=0,d3=0,d4=0,d5=0,d6=0,seed;
+	scanf("%d",&seed);
+	srand (seed);
+	for (i=1;i<=10;i++)
+	{
+		me = rand()%6+1;
+		if(me == 6) d6++;
+		if(me == 5) d5++;
+		if(me == 4) d4++;
+		if(me == 3) d3++;
+		if(me == 2) d2++;
+		if(me == 1) d1++;
+	}
+	printf ("%d %d %d %d %d %d\n",d1,d2,d3,d4,d5,d6);
+	return 0;
+}
